29ad5a119f9771f76135b2831d4ca2da22ca1156
kozue-y <sep_04_17_jaskoz@icloud.com>
2026-05-04 01:37:22 +0900

    ファビコン管理画面、パンくず1行スクロール、TOP（voice、ブログ）行数制限追加

--- header.php
+++ header.php
@@ -5,7 +5,6 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="icon" href="<?php echo get_template_directory_uri(); ?>/images/sitetitle_icon.svg" type="image/svg">
     <meta name="robots" content="noindex, nofollow">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
